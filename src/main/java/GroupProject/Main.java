9fdfd0b509457a734df1aa8c7c08819652ed9ca8
johnfa1508 <johnfa@hiof.no>
2021-10-21 15:27:39 +0200

    Made testFile to debug

--- src/main/java/GroupProject/Main.java
+++ src/main/java/GroupProject/Main.java
@@ -7,7 +7,10 @@ import GroupProject.repository.AntiqueRepository;
 public class Main {
     public static void main(String[] args) {
         // Make JSON-file and controller to access commands for file
-        AntiqueRepository antiqueRepository = new AntiqueJSONRepository("antiques.json");
+//        AntiqueRepository antiqueRepository = new AntiqueJSONRepository("antiques.json");
+
+        // Repository for debugging
+        AntiqueRepository antiqueRepository = new AntiqueJSONRepository("testFile.json");
         AntiqueController antiqueController = new AntiqueController(antiqueRepository);
 
         // Go to login-screen
