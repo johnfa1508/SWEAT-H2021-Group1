cdc2475ac185dc3a8dc224935df66614b2bf8ce3
johnfa1508 <johnfa@hiof.no>
2021-11-30 17:50:50 +0100

    Added tests

--- src/test/java/User_Tests.java
+++ src/test/java/User_Tests.java
@@ -3,20 +3,26 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import GroupProject.repository.*;
 
-import java.util.HashMap;
-
 public class User_Tests {
     // Use usersTest.json as test-file
     UserRepository userRepository = new UserJSONRepository("usersTest.json");
     // Make new user for testing
     User newUser = new User("userTest", 100);
 
-    // TEST IF NEW USER EXISTS IN REPOSITORY AFTER MAKING NEW USER AND ADDING IT
+    // TEST IF userExists()-FUNCTION RETURNS TRUE IF USER EXISTS
     @Test
-    public void New_user_exists() {
+    public void Return_true_if_user_exists() {
         userRepository.addUser(newUser);
 
         Assertions.assertTrue(userRepository.userExists(newUser.getName()));
+    }
+
+    // TEST IF userExists()-FUNCTION RETURNS FALSE IF USER DOES NOT EXIST
+    @Test
+    public void Return_false_if_user_does_not_exists() {
+        userRepository.removeUser(newUser);
+
+        Assertions.assertFalse(userRepository.userExists(newUser.getName()));
     }
 
     // TEST IF NEW USER GETS RETURNED WHEN USING getUser(String userKey)-FUNCTION IN UserJSONRepository
@@ -27,9 +33,9 @@ public class User_Tests {
         Assertions.assertNotNull(userRepository.getUser(newUser.getName()));
     }
 
-    // TESTS IF PROGRAM RETURNS NULL IF USER-REPOSITORY IS EMPTY
+    // TESTS IF PROGRAM RETURNS NOTHING IF USER-REPOSITORY IS EMPTY
     @Test
-    public void Return_Null_If_User_Repository_Is_Empty() {
+    public void Return_empty_userMap_if_user_repository_is_empty() {
         userRepository.removeUser(newUser);
 
         Assertions.assertTrue(userRepository.showUsers().isEmpty());
@@ -37,7 +43,21 @@ public class User_Tests {
 
     // TESTS IF PROGRAM RETURNS VALUES IF USER-REPOSITORY IS NOT EMPTY
     @Test
-    public void Return_values_if_user_repository_is_not_empty(){
+    public void Return_values_if_user_repository_is_not_empty() {
         Assertions.assertNotNull(userRepository.showUsers());
+    }
+
+    // TESTS IF PROGRAM RETURNS NOTHING IF USER-REPOSITORY IS EMPTY
+    @Test
+    public void Return_empty_userNameMap_if_user_repository_is_empty() {
+        userRepository.removeUser(newUser);
+
+        Assertions.assertTrue(userRepository.showUserNames().isEmpty());
+    }
+
+    // TESTS IF PROGRAM RETURNS USERNAMES IF USER-REPOSITORY IS NOT EMPTY
+    @Test
+    public void Return_usernames_if_user_repository_is_not_empty() {
+        Assertions.assertNotNull(userRepository.showUserNames());
     }
 }
